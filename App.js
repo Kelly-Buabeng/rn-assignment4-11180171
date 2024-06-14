fbc353510f06653962ec85678b7c98784d1a249a
Kelly Buabeng <buabengkelly@gmail.com>
2024-06-14 20:44:10 +0000

    created the various pages and added the needed assets

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'yellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  MagnifyingGlassIcon: {
+    color: 'black',
+    top:14,
+    left: 12,
+  }
 });
